3e242743b00fcde87e131d1f5a0c251723b14818
DMDD <DMDD@DMDD-1-Win7>
2012-12-14 10:43:40 -0600

    12/13/2012 - rest update

---
+++ src/main/java/cdiddy/utils/system/DataRequestCaller.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cdiddy.utils.system;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ *
+ * @author DMDD
+ */
+public class DataRequestCaller {
+    
+    public static Object requestData(String requestURL, String method)
+    {
+        
+        try
+        {
+            URL url = new URL(requestURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod(method);
+            conn.setRequestProperty("Accept", "application/json");
+            if (conn.getResponseCode() != 200) 
+            {
+                    throw new RuntimeException("Failed : HTTP error code : "
+                                    + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            String result = "";
+            //System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                  result += output;  
+            }
+
+            conn.disconnect();
+            return result;
+        }
+        catch (Exception e)
+        {
+          return null;
+        }
+        
+    
+    }
+    
+}
